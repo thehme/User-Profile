3fe6b33ab6f706d3f58be43f8e38322a5788e656
thehme <bcortez23@gmail.com>
2018-05-21 22:46:05 -0400

    added use of setTextSize and setTextColor

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.userprofile;
 
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,11 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         TextView nameTextView = (TextView) findViewById(R.id.name);
         nameTextView.setText(R.string.name);
+        nameTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 32);
+        nameTextView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
 
         TextView birthdayTextView = (TextView) findViewById(R.id.birthday);
         birthdayTextView.setText(R.string.bday);
+        birthdayTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
 
         TextView countryTextView = (TextView) findViewById(R.id.country);
         countryTextView.setText(R.string.country);
+        countryTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
     }
 }
