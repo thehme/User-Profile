7fd7b67b090a35b9a9f237f9e91b47dce13a6d79
thehme <bcortez23@gmail.com>
2018-05-21 22:14:59 -0400

    adding project files

---
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.userprofile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView profileImage = (ImageView) findViewById(R.id.profile_picture);
+        profileImage.setImageResource(R.drawable.avatar);
+
+        TextView nameTextView = (TextView) findViewById(R.id.name);
+        nameTextView.setText(R.string.name);
+
+        TextView birthdayTextView = (TextView) findViewById(R.id.birthday);
+        birthdayTextView.setText(R.string.bday);
+
+        TextView countryTextView = (TextView) findViewById(R.id.country);
+        countryTextView.setText(R.string.country);
+    }
+}
